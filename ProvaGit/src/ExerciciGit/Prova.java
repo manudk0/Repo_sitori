9b97e9380be08ad5ec3a8e90a61315c4b73542c3
Manu_dk1 <Manu_dk1@TEKU1>
2015-01-16 16:18:31 +0100

    probando

---
+++ ProvaGit/src/ExerciciGit/Prova.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ExerciciGit;
+
+/**
+ * @author Manu_dk1
+ *
+ */
+public class Prova {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola");
+
+	}
+
+}
