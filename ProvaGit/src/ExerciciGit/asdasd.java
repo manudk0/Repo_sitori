1f28b79214336341e0c855e8270e5ea9c30a32fd
Manu_dk1 <Manu_dk1@TEKU1>
2015-01-20 20:05:02 +0100

    sdfsd

---
+++ ProvaGit/src/ExerciciGit/asdasd.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ExerciciGit;
+
+/**
+ * @author Manu_dk1
+ *
+ */
+public class asdasd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mon");
+
+	}
+
+}
