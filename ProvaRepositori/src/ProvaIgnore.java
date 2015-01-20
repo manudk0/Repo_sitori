8c2e9c8482451071208922465b77a74dcbc1f3b4
Manu_dk1 <Manu_dk1@TEKU1>
2015-01-20 20:26:13 +0100

    ff

---
+++ ProvaRepositori/src/ProvaIgnore.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Manu_dk1
+ *
+ */
+public class ProvaIgnore {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
